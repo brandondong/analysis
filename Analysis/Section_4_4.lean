d76dbdec674cbaa0b8227059c74e184416238cd4
Brandon <brandondong96@gmail.com>
2026-01-24 00:48:00 -0800

    Finish 4_4

--- Analysis/Section_4_4.lean
+++ Analysis/Section_4_4.lean
@@ -26,48 +26,95 @@ Users of the companion who have completed the exercises in this section are welc
 
 /-- Proposition 4.4.1 (Interspersing of integers by rationals) / Exercise 4.4.1 -/
 theorem Rat.between_int (x:ℚ) : ∃! n:ℤ, n ≤ x ∧ x < n+1 := by
-  sorry
-
-theorem Nat.exists_gt (x:ℚ) : ∃ n:ℕ, n > x := by
-  -- If x is non-negative, then just consider the numerator+1.
-  obtain h | h := lt_trichotomy x 0
-  . use 0
-    linarith
-  replace h : 0 ≤ x
-  . apply le_iff_eq_or_lt.mpr
-    tauto
-  obtain ⟨ a, b, hb, hc ⟩ := x
-  rw [← Rat.num_nonneg] at h
-  simp at h
-  match a with
-  | .ofNat a =>
-    use (a+1)
-    rw [gt_iff_lt, lt_iff_exists_pos_add]
-    use mkRat (b*a - a + b) b
-    -- Convert everything to mkRat?
-    have : ↑(a + 1) = mkRat (a+1) 1
-    . simp
-    rw [this]; clear this
-    have : { num := Int.ofNat a, den := b, den_nz := hb, reduced := hc } = mkRat a b
-    . simp
-      exact Rat.mk_eq_mkRat (↑a) b hb _
-    rw [this]; clear this
+  -- For a/b, consider floor(a/b).
+  -- We need to show inductively this value exists and has b*x+r = a.
+  apply existsUnique_of_exists_of_unique
+  . have hb := Rat.den_ne_zero x
+    have he : ∀ a:ℤ, ∃ z:ℤ, ∃ r:ℕ, x.den * z + r = a ∧ r < x.den
+    . set b := x.den
+      intro a
+      match a with
+      | .ofNat a =>
+        simp
+        induction' a with a IH
+        . use 0, 0
+          simp
+          omega
+        obtain ⟨ z, r, h1, h2 ⟩ := IH
+        by_cases h3 : r + 1 = b
+        . use (z+1), 0
+          simp
+          constructor
+          . linarith
+          . omega
+        . use z, r+1
+          constructor
+          . simp
+            linarith
+          . omega
+      | .negSucc a =>
+        induction' a with a IH
+        . use -1, b-1
+          omega
+        obtain ⟨ z, r, h1, h2 ⟩ := IH
+        by_cases h3 : r = 0
+        . use (z-1), b-1
+          constructor
+          . simp [h3] at h1
+            ring_nf
+            omega
+          . omega
+        . use z, r-1
+          constructor <;> omega
+    obtain ⟨ z, r, h1, h2 ⟩ := he x.num
+    use z
+    rw [← Rat.mkRat_one, ← Rat.mkRat_self x]
     constructor
-    . rw [Rat.mkRat_pos_iff _ hb]
-      replace hb : 0 < (b:ℤ) := by omega
-      have goal : (b:ℤ) * ↑a ≥ ↑a
-      . have h1 : (1:ℤ) * ↑a ≥ ↑a := by omega
-        exact le_mul_of_le_mul_of_nonneg_right h1 hb h
-      omega
-    . have hbb : b * b ≠ 0
-      . contrapose! hb
-        simp at hb
-        exact hb
-      rw [Rat.mkRat_add_mkRat _ _ hb hb, Rat.mkRat_eq_iff hbb (by omega)]
+    . rw [le_iff_exists_nonneg_add]
+      use mkRat r x.den
+      constructor
+      . rw [mkRat_nonneg_iff _ hb]
+        omega
+      rw [Rat.mkRat_add_mkRat _ _ (by omega) hb, Rat.mkRat_eq_iff (by omega) hb]
       simp
       linarith
-  | .negSucc a =>
-    omega
+    . rw [lt_iff_exists_pos_add]
+      use mkRat (x.den - r) x.den
+      constructor
+      . rw [Rat.mkRat_pos_iff _ hb]
+        linarith
+      have : mkRat 1 1 = 1 := by rfl
+      rw [← this]; clear this
+      rw [Rat.mkRat_add_mkRat_of_den _ _ hb, Rat.mkRat_add_mkRat_of_den _ _ (by omega)]
+      rw [Rat.mkRat_eq_iff hb (by omega)]
+      simp
+      linarith
+  . intro n1 n2 hn1 hn2
+    by_contra hn
+    -- wlog, n1 < n2. Then n1+1 <= n2.
+    wlog h : n1 < n2
+    . replace h : n2 < n1 := by omega
+      exact this x n2 n1 hn2 hn1 (by tauto) h
+    replace h : n1+1 ≤ n2 := by omega
+    replace h : (n1:ℚ) +1 ≤ n2
+    . norm_cast
+    linarith
+
+theorem Nat.exists_gt (x:ℚ) : ∃ n:ℕ, n > x := by
+  obtain h | h := lt_or_ge x 0
+  . use 0
+    linarith
+  obtain ⟨ n, ⟨ _, hn ⟩, _ ⟩ := Rat.between_int x
+  match n with
+  | .ofNat n =>
+    use (n+1)
+    simp at hn
+    norm_cast at hn
+  | .negSucc n =>
+    have : ((Int.negSucc n):ℚ) ≤ -1
+    . norm_cast
+      omega
+    linarith
 
 /-- Proposition 4.4.3 (Interspersing of rationals) -/
 theorem Rat.exists_between_rat {x y:ℚ} (h: x < y) : ∃ z:ℚ, x < z ∧ z < y := by
@@ -190,10 +237,30 @@ theorem Rat.not_exist_sqrt_two : ¬ ∃ x:ℚ, x^2 = 2 := by
       choose q hpos hq using hPp.2
       have : q^2 = 2 * k^2 := by linarith
       use q; constructor
-      . sorry
+      . by_contra contra
+        simp at contra
+        have h1 : q ^ 2 ≥ 4 * k ^ 2
+        . have : q ^ 2 = q * q := by ring
+          rw [this]
+          have : 4 * k ^ 2 = (2 * k) * (2 * k) := by ring
+          rw [this]
+          exact Nat.mul_le_mul contra contra
+        have h2 : k > 0
+        . by_contra hk
+          replace hk : k = 0 := by omega
+          simp [hk] at this
+          omega
+        rw [this] at h1
+        have h3 : k ^ 2 > 0
+        . exact Nat.pow_pos h2
+        linarith
       exact ⟨ hpos, k, by linarith [hPp.1], this ⟩
     have h1 : Odd (p^2) := by
-      sorry
+      rw [odd_iff_exists_bit1] at hp
+      obtain ⟨ x, rfl ⟩ := hp
+      rw [odd_iff_exists_bit1]
+      use 2*x^2 + 2*x
+      ring
     have h2 : Even (p^2) := by
       choose q hpos hq using hPp.2
       rw [even_iff_exists_two_mul]
