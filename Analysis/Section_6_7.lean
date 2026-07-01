663ac7209e06a5b2af31abe175a07125c53f9804
Brandon <brandondong96@gmail.com>
2026-07-01 00:08:59 -0700

    Start 6_7

--- Analysis/Section_6_7.lean
+++ Analysis/Section_6_7.lean
@@ -147,7 +147,20 @@ lemma Real.rpow_of_rat_eq_ratPow {x:ℝ} (hx: x > 0) {q: ℚ} :
 
 /-- Proposition 6.7.3(a) / Exercise 6.7.1 -/
 theorem Real.ratPow_nonneg {x:ℝ} (hx: x > 0) (q:ℝ) : rpow x q ≥ 0 := by
-  sorry
+  obtain ⟨ b, hb ⟩ := Real.eq_lim_of_rat q
+  have := Real.rpow_eq_lim_ratPow hx hb
+  rw [this]; clear this
+  obtain ⟨ L, hL ⟩ := ratPow_continuous hx hb
+  have := (Sequence.lim_eq.mp hL).2
+  rw [this]; clear this
+  rw [Sequence.tendsTo_coe] at hL
+  contrapose! hL
+  use -L, (by linarith)
+  intro n
+  use n, (by simp)
+  have h : x ^ ((b n):ℝ) > 0 := by exact rpow_pos_of_pos hx ↑(b n)
+  rw [abs_of_nonneg (by linarith)]
+  linarith
 
 /-- Proposition 6.7.3(b) -/
 theorem Real.ratPow_add {x:ℝ} (hx: x > 0) (q r:ℝ) : rpow x (q+r) = rpow x q * rpow x r := by
@@ -167,13 +180,159 @@ theorem Real.ratPow_add {x:ℝ} (hx: x > 0) (q r:ℝ) : rpow x (q+r) = rpow x q 
 theorem Real.ratPow_ratPow {x:ℝ} (hx: x > 0) (q r:ℝ) : rpow (rpow x q) r = rpow x (q*r) := by
   sorry
 
+theorem rpow_bounded_not_zero_helper {x:ℝ} {a : ℕ → ℚ} (hx: x > 0) (h : ((fun n ↦ ((a n):ℝ)):Sequence).IsBounded) : ¬((fun n ↦ x ^ ((a n):ℝ)):Sequence).TendsTo 0 := by
+  obtain ⟨ M, hM, h ⟩ := h
+  rw [boundedBy_def_coe hM] at h
+  rw [tendsTo_coe]
+  push_neg
+  obtain hx2 | rfl | hx2 := lt_trichotomy x 1
+  . use x^(M+1)
+    constructor
+    . apply rpow_pos_of_pos hx
+    intro n
+    use n, (by simp)
+    simp
+    rw [abs_of_nonneg (by apply rpow_nonneg; linarith)]
+    specialize h n
+    rw [abs_le] at h
+    replace h : a n < M+1 := by linarith
+    exact (rpow_lt_rpow_left_iff_of_base_lt_one hx hx2).mpr h
+  . use 0.5, (by norm_num)
+    intro n
+    use n, (by simp)
+    simp
+    norm_num
+  . use x^(-M-1), (by apply rpow_pos_of_pos hx)
+    intro n
+    use n, (by simp)
+    simp
+    rw [abs_of_nonneg (by apply rpow_nonneg; linarith)]
+    specialize h n
+    rw [abs_le] at h
+    replace h : -M-1 < a n := by linarith
+    exact (rpow_lt_rpow_left_iff hx2).mpr h
+
 /-- Proposition 6.7.3(c) / Exercise 6.7.1 -/
 theorem Real.ratPow_neg {x:ℝ} (hx: x > 0) (q:ℝ) : rpow x (-q) = 1 / rpow x q := by
-  sorry
+  obtain ⟨ a, ha ⟩ := Real.eq_lim_of_rat q
+  have := Real.rpow_eq_lim_ratPow hx ha
+  rw [this]; clear this
+  have ha2 := Sequence.tendsTo_smul (-1:ℝ) ha
+  have : -1 * q = -q := by ring
+  rw [this, Sequence.smul_coe] at ha2; clear this
+  set b := fun n ↦ (-a n)
+  have : (fun n ↦ (-1:ℝ) * ((a n):ℝ)) = (fun n ↦ ((b n):ℝ))
+  . simp [b]
+  rw [this] at ha2; clear this
+  have := Real.rpow_eq_lim_ratPow hx ha2
+  rw [this]; unfold b; clear this ha2 b
+  set b := ((fun n ↦ x ^ ((a n):ℝ)):Sequence)
+  have : (fun n ↦ x ^ (((-a n):ℚ):ℝ)) = b⁻¹
+  . simp [b]
+    ext n
+    . rfl
+    simp
+    by_cases hn : 0 ≤ n <;> simp [hn]
+    . lift n to ℕ using hn
+      simp
+      set c := ((a n):ℝ)
+      apply rpow_neg
+      linarith
+  rw [this]; clear this
+  have : 1 / lim b = (lim b)⁻¹ := one_div _
+  rw [this]; clear this
+  have hb : b.Convergent
+  . simp [b]
+    exact ratPow_continuous hx ha
+  have hb2 : lim b ≠ 0
+  . by_contra h
+    replace h : b.TendsTo 0
+    . rw [Sequence.lim_eq]
+      simp [hb, h]
+    contrapose! h
+    unfold b
+    rw [Sequence.lim_eq] at ha
+    replace h := Sequence.bounded_of_convergent ha.1
+    exact rpow_bounded_not_zero_helper hx h
+  exact (Sequence.lim_inv hb hb2).2
+
+theorem gt_exists_sequence_gt {q:ℝ} {a : ℕ → ℚ} (hq: q > 0) (ha : ((fun n ↦ ((a n):ℝ)):Sequence).TendsTo q) : ∃ m, ∀ m' ≥ m, a m' > 0 := by
+  rw [tendsTo_coe] at ha
+  specialize ha (q/2) (by linarith)
+  obtain ⟨ N, hN ⟩ := ha
+  use N
+  intro n hn
+  specialize hN n hn
+  rw [abs_le] at hN
+  suffices h : ((a n):ℝ) > 0
+  . norm_cast at h
+  linarith
 
 /-- Proposition 6.7.3(d) / Exercise 6.7.1 -/
-theorem Real.ratPow_mono {x y:ℝ} (hx: x > 0) (hy: y > 0) {q:ℝ} (h: q > 0) : x > y ↔ rpow x q > rpow y q := by
-  sorry
+theorem Real.ratPow_mono {x y:ℝ} (hx: x > 0) (hy: y > 0) {q:ℝ} (hq: q > 0) : x > y ↔ rpow x q > rpow y q := by
+  obtain ⟨ a, ha ⟩ := Real.eq_lim_of_rat q
+  have hxq := Real.rpow_eq_lim_ratPow hx ha
+  have hyq := Real.rpow_eq_lim_ratPow hy ha
+  rw [hxq, hyq]; clear hxq hyq
+  set c := ((fun n ↦ y ^ ((a n):ℝ)):Sequence)
+  set d := (fun n ↦ x ^ ((a n):ℝ):Sequence)
+  have hc : c.Convergent
+  . exact ratPow_continuous hy ha
+  have hd : d.Convergent
+  . exact ratPow_continuous hx ha
+  constructor <;> intro h
+  . suffices h : 0 < (lim d) - (lim c)
+    . linarith
+    obtain ⟨ hcd, hcd2 ⟩ := (Sequence.LIM_sub hd hc)
+    obtain ⟨ L, hL ⟩ := hcd
+    have hL2 := (Sequence.lim_eq.mp hL).2
+    rw [← hcd2, hL2]; clear hL2 hcd2
+    have : d - c = (fun n ↦ x ^ ((a n):ℝ) - y ^ ((a n):ℝ))
+    . unfold c d
+      ext n
+      . rfl
+      simp
+      by_cases hn : 0 ≤ n <;> simp [hn]
+    rw [this] at hL; clear this
+    rw [tendsTo_coe] at hL
+    contrapose! hL
+    -- We can find some n where all n'>= n has a n' >= q/2.
+    -- Then x^an'-y^an' >= x^an-y^an.
+    sorry
+  . contrapose! h
+    -- Need to prove 0 <= L = lim y^an - x^an
+    suffices h : 0 ≤ (lim c) - (lim d)
+    . linarith
+    obtain ⟨ hcd, hcd2 ⟩ := (Sequence.LIM_sub hc hd)
+    obtain ⟨ L, hL ⟩ := hcd
+    have hL2 := (Sequence.lim_eq.mp hL).2
+    rw [← hcd2, hL2]; clear hL2 hcd2
+    -- At a certain m, all m' >= m are > 0.
+    obtain ⟨ m, hm ⟩ := gt_exists_sequence_gt hq ha
+    -- If L < 0, then cannot be arbitrarily close to L/2 because x^an <= y^an.
+    have : c - d = (fun n ↦ y ^ ((a n):ℝ) - x ^ ((a n):ℝ))
+    . unfold c d
+      ext n
+      . rfl
+      simp
+      by_cases hn : 0 ≤ n <;> simp [hn]
+    rw [this] at hL; clear this
+    rw [tendsTo_coe] at hL
+    contrapose! hL
+    use -L/2, (by linarith)
+    intro n
+    use max m n, (by simp)
+    set e := ((a (max m n)):ℝ)
+    have : x ^ e ≤ y ^ e
+    . rw [rpow_le_rpow_iff]
+      . exact h
+      . linarith
+      . linarith
+      . unfold e
+        specialize hm (max m n) (by simp)
+        norm_cast
+    rw [abs_of_nonneg (by linarith)]
+    linarith
 
 /-- Proposition 6.7.3(e) / Exercise 6.7.1 -/
 theorem Real.ratPow_mono_of_gt_one {x:ℝ} (hx: x > 1) {q r:ℝ} : rpow x q > rpow x r ↔ q > r := by
