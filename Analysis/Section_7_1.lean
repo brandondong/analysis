2da62fc1ace8be0689b439f96b535a813193864a
Brandon <brandondong96@gmail.com>
2026-07-25 00:00:39 -0700

    Type conversion nonsense

--- Analysis/Section_7_1.lean
+++ Analysis/Section_7_1.lean
@@ -118,6 +118,27 @@ theorem shift_finite_series {m n k:ℤ} (a: ℤ → ℝ) :
   have : m + ↑c + k + 1 - k = m + ↑c + 1 := by ring
   rw [this]
 
+theorem eq_finite_series {m n:ℤ} {a b: ℤ → ℝ} (h: ∀ (i:ℤ), m ≤ i ∧ i ≤ n → a i = b i) :
+  ∑ i ∈ Icc m n, a i = ∑ i ∈ Icc m n, b i := by
+  obtain hnm | hnm := lt_or_ge n m
+  . simp [sum_of_empty hnm]
+  rw [le_iff_exists_nonneg_add] at hnm
+  obtain ⟨ c, hc, rfl ⟩ := hnm
+  lift c to ℕ using hc
+  induction' c with c IH
+  . simp
+    exact h m (by simp)
+  specialize IH (by {
+    intro i hi
+    apply h
+    omega
+  })
+  have : m + ((c + 1):ℕ) = (m + c) + 1 := by omega
+  rw [this]; clear this
+  rw [sum_of_nonempty (by omega), IH, sum_of_nonempty (by omega)]
+  specialize h (m+c+1) (by omega)
+  linarith
+
 /-- Lemma 7.1.4(c) / Exercise 7.1.1 -/
 theorem finite_series_add {m n:ℤ} (a b: ℤ → ℝ) :
   ∑ i ∈ Icc m n, (a i + b i) = ∑ i ∈ Icc m n, a i + ∑ i ∈ Icc m n, b i := by
@@ -186,8 +207,6 @@ theorem finite_series_of_le {m n:ℤ}  {a b: ℤ → ℝ} (h: ∀ i, m ≤ i →
   rw [this, sum_of_nonempty (by omega), sum_of_nonempty (by omega)]
   specialize h (m+d+1) (by omega) (by omega)
   linarith
-
-#check sum_congr
 
 set_option maxHeartbeats 210000 in
 /--
@@ -334,29 +353,210 @@ theorem finite_series_of_fintype {X':Type*} (f: X' → ℝ) (X: Finset X') :
 /-- Proposition 7.1.11(c) / Exercise 7.1.2 -/
 theorem map_finite_series {X:Type*} [Fintype X] [Fintype Y] (f: X → ℝ) {g:Y → X}
   (hg: Function.Bijective g) :
-    ∑ x, f x = ∑ y, f (g y) := by sorry
+    ∑ x, f x = ∑ y, f (g y) := by
+  -- We only know how to talk about sum of elems in Finset instead of being part of a Fintype...
+  rw [finite_series_of_fintype]
+  obtain ⟨ fx, hfx ⟩ := exist_bijection (n := univ.card) (Y := X) (univ) (by rfl)
+  have h1 := finite_series_eq (n := univ.card) (Y := X) (univ) f fx hfx
+  rw [finite_series_of_fintype] at h1
+  rw [h1]; clear h1
+  rw [finite_series_of_fintype (X' := Y)]
+  obtain ⟨ fy, hfy ⟩ := exist_bijection (n := univ.card) (Y := Y) (univ) (by rfl)
+  have h2 := finite_series_eq (n := univ.card) (Y := Y) (univ) (f ∘ g) fy hfy
+  rw [finite_series_of_fintype] at h2
+  rw [Function.comp_def] at h2
+  rw [h2]; clear h2
+  simp only [card_univ]
+  have hc : Fintype.card X = Fintype.card Y
+  . exact (Fintype.card_of_bijective hg).symm
+  simp_rw [← hc]
+  -- Use finite_series_of_rearrange.
+  -- We can find two bijective mappings from Z -> X (map to X, map to g Y).
+  set g' : (Icc 1 ((Fintype.card X):ℤ)) → (univ : Finset X) := fun i ↦
+    let v := fy ⟨ i, (by rw [card_univ, ← hc]; simp) ⟩ ;
+    let v2 := g v
+    ⟨ v2, (by simp) ⟩
+  have hg' : Function.Bijective g'
+  . constructor
+    . intro a b h
+      simp [g'] at h
+      replace h := hg.1 h
+      simp at h
+      replace h := hfy.1 h
+      simp at h
+      exact h
+    . intro b
+      simp only [g']
+      obtain ⟨ a, ha ⟩ := hg.2 b
+      obtain ⟨ i, hi ⟩ := hfy.2 (⟨ a, (by simp) ⟩)
+      use ⟨ i, by {
+        have hi := i.2
+        simp_rw [card_univ, ← hc] at hi
+        exact hi
+      } ⟩
+      simp [hi, ha]
+  have := finite_series_of_rearrange (n := Fintype.card X) (X' := X) (univ) (by rfl) f fx (g') hfx hg'
+  rw [this]; clear this
+  simp [g']
 
 -- Proposition 7.1.11(d) is `rfl` in our formalism and is therefore omitted.
 
 /-- Proposition 7.1.11(e) / Exercise 7.1.2 -/
 theorem finite_series_of_disjoint_union {Z:Type*} {X Y: Finset Z} (hdisj: Disjoint X Y) (f: Z → ℝ) :
-    ∑ z ∈ X ∪ Y, f z = ∑ z ∈ X, f z + ∑ z ∈ Y, f z := by sorry
+    ∑ z ∈ X ∪ Y, f z = ∑ z ∈ X, f z + ∑ z ∈ Y, f z := by
+  have hc : (X ∪ Y).card = (X.card + Y.card) := card_union_of_disjoint hdisj
+  obtain ⟨ fx, hfx ⟩ := exist_bijection (n := X.card) (Y := Z) (X) (by rfl)
+  have := finite_series_eq (n := X.card) (Y := Z) (X) f fx hfx
+  rw [this]; clear this
+  obtain ⟨ fy, hfy ⟩ := exist_bijection (n := Y.card) (Y := Z) (Y) (by rfl)
+  have := finite_series_eq (n := Y.card) (Y := Z) (Y) f fy hfy
+  rw [this]; clear this
+  -- There exists bijective f' where 1-n map to X via fx and n+1-n+m map to Y via fy.
+  -- concat_finite_series + shift, then set to equal both sides of +.
+  set fxy : ↥(Icc 1 (((X.card + Y.card):ℕ):ℤ)) → ↥(X ∪ Y) := fun i ↦
+    if hi:i.val ≤ X.card then
+      let v := fx ⟨ i, (by {
+        have hi2 := i.2
+        set c := i.val
+        simp at hi2
+        simp [hi, hi2]
+      }) ⟩;
+      ⟨ v, (by simp) ⟩
+    else
+      let v := fy ⟨ i-X.card, (by {
+        have hi2 := i.2
+        set c := i.val
+        simp at hi2
+        simp
+        omega
+      }) ⟩;
+      ⟨ v, (by simp) ⟩
+  have hfxy : Function.Bijective fxy
+  . constructor
+    . intro i1 i2 h
+      simp [fxy] at h
+      have hii : i1.val ≤ X.card ↔ i2.val ≤ X.card
+      . wlog hi : i1.val ≤ X.card generalizing i1 i2
+        . simp only [hi, false_iff]
+          have hii := this (i1 := i2) (i2 := i1) h.symm
+          by_contra hi2
+          exact hi ((hii hi2).mp hi2)
+        simp [hi]
+        by_contra hi2
+        simp [hi, hi2] at h
+        have hc (x: X) (y: Y) : x.val ≠ y.val
+        . by_contra h
+          have hy := y.2
+          rw [← h] at hy
+          contrapose! hy
+          have hx := x.2
+          exact Disjoint.notMem_of_mem_left_finset hdisj hx
+        tauto
+      by_cases hi1 : i1.val ≤ X.card
+      . have hi2 := hii.mp hi1
+        simp [hi1, hi2] at h
+        have := hfx.1 h
+        simp at this
+        exact this
+      . simp only [hi1, false_iff] at hii
+        simp [hi1, hii] at h
+        have := hfy.1 h
+        simp at this
+        exact this
+    . intro ⟨ z, hz ⟩
+      simp at hz
+      simp only [fxy]
+      obtain hz | hz := hz
+      . obtain ⟨ ⟨ i, hi ⟩, hfi ⟩ := hfx.2 ⟨ z, hz ⟩
+        simp at hi
+        use ⟨ i, by simp; omega ⟩
+        simp [hi, hfi]
+      . obtain ⟨ ⟨ i, hi ⟩, hfi ⟩ := hfy.2 ⟨ z, hz ⟩
+        simp at hi
+        use ⟨ i+X.card, (by simp; omega) ⟩
+        have hi2 : ¬ i ≤ 0 := by omega
+        simp [hi2, hfi]
+  have := finite_series_eq (n := (X.card + Y.card)) (Y := Z) (X ∪ Y) f fxy hfxy
+  rw [this]; clear this
+  have := concat_finite_series (m := 1) (n := X.card) (p := X.card + Y.card) (by omega) (by omega)
+  have hc2 : ((X.card + Y.card):ℕ) = (X.card:ℤ) + Y.card := by omega
+  simp_rw [hc2];
+  rw [← concat_finite_series (m := 1) (n := X.card) (p := X.card + Y.card) (by omega) (by omega)]
+  have split (a b c d:ℝ) (h1: a = b) (h2: c = d) : a + c = b + d := by linarith
+  apply split <;> clear split
+  . apply eq_finite_series
+    intro i ⟨ hi1, hi2 ⟩
+    simp [hi1, hi2]
+    have : i ≤ ↑(#X) + ↑(#Y) := by omega
+    simp [this, fxy, hi2]
+  . rw [shift_finite_series (m := 1) (n := Y.card) (k := X.card)]
+    rw [add_comm]
+    have hc3 : Y.card + X.card = (X.card:ℤ) + Y.card := by omega
+    rw [hc3]; clear hc3
+    apply eq_finite_series
+    intro i ⟨ hi1, hi2 ⟩
+    have h1 : 1 ≤ i ∧ i ≤ ↑(#X) + ↑(#Y) := by omega
+    have h2 : 1 ≤ i - ↑(#X) ∧ i ≤ ↑(#Y) + ↑(#X) := by omega
+    have h3 : ¬ i ≤ ↑(#X) := by omega
+    simp [h1, h2, fxy, h3]
 
 /-- Proposition 7.1.11(f) / Exercise 7.1.2 -/
 theorem finite_series_of_add {X':Type*} (f g: X' → ℝ) (X: Finset X') :
-    ∑ x ∈ X, (f + g) x = ∑ x ∈ X, f x + ∑ x ∈ X, g x := by sorry
+    ∑ x ∈ X, (f + g) x = ∑ x ∈ X, f x + ∑ x ∈ X, g x := by
+  obtain ⟨ fx, hfx ⟩ := exist_bijection (n := X.card) (Y := X') (X) (by rfl)
+  have h1 := finite_series_eq (n := X.card) (Y := X') (X) f fx hfx
+  have h2 := finite_series_eq (n := X.card) (Y := X') (X) g fx hfx
+  have h3 := finite_series_eq (n := X.card) (Y := X') (X) (f+g) fx hfx
+  rw [h1, h2, h3]; clear h1 h2 h3
+  rw [← finite_series_add]
+  apply eq_finite_series
+  intro i hi
+  have hi2 : i ∈ Icc 1 ↑(#X)
+  . simp [hi]
+  simp [hi2]
 
 /-- Proposition 7.1.11(g) / Exercise 7.1.2 -/
 theorem finite_series_of_const_mul {X':Type*} (f: X' → ℝ) (X: Finset X') (c:ℝ) :
-    ∑ x ∈ X, c * f x = c * ∑ x ∈ X, f x := by sorry
+    ∑ x ∈ X, c * f x = c * ∑ x ∈ X, f x := by
+  obtain ⟨ fx, hfx ⟩ := exist_bijection (n := X.card) (Y := X') (X) (by rfl)
+  have h1 := finite_series_eq (n := X.card) (Y := X') (X) f fx hfx
+  have h2 := finite_series_eq (n := X.card) (Y := X') (X) (fun x ↦ c * f x) fx hfx
+  rw [h1, h2]; clear h1 h2
+  rw [← finite_series_const_mul]
+  apply eq_finite_series
+  intro i hi
+  have hi2 : i ∈ Icc 1 ↑(#X)
+  . simp [hi]
+  simp [hi2]
 
 /-- Proposition 7.1.11(h) / Exercise 7.1.2 -/
 theorem finite_series_of_le' {X':Type*} (f g: X' → ℝ) (X: Finset X') (h: ∀ x ∈ X, f x ≤ g x) :
-    ∑ x ∈ X, f x ≤ ∑ x ∈ X, g x := by sorry
+    ∑ x ∈ X, f x ≤ ∑ x ∈ X, g x := by
+  obtain ⟨ fx, hfx ⟩ := exist_bijection (n := X.card) (Y := X') (X) (by rfl)
+  have h1 := finite_series_eq (n := X.card) (Y := X') (X) f fx hfx
+  have h2 := finite_series_eq (n := X.card) (Y := X') (X) g fx hfx
+  rw [h1, h2]; clear h1 h2
+  apply finite_series_of_le
+  intro i hi hi2
+  simp [hi, hi2]
+  apply h
+  simp
 
 /-- Proposition 7.1.11(i) / Exercise 7.1.2 -/
 theorem abs_finite_series_le' {X':Type*} (f: X' → ℝ) (X: Finset X') :
-    |∑ x ∈ X, f x| ≤ ∑ x ∈ X, |f x| := by sorry
+    |∑ x ∈ X, f x| ≤ ∑ x ∈ X, |f x| := by
+  obtain ⟨ fx, hfx ⟩ := exist_bijection (n := X.card) (Y := X') (X) (by rfl)
+  have h1 := finite_series_eq (n := X.card) (Y := X') (X) f fx hfx
+  have h2 := finite_series_eq (n := X.card) (Y := X') (X) (fun x ↦ |f x|) fx hfx
+  rw [h1, h2]; clear h1 h2
+  have := abs_finite_series_le (m := 1) (n := X.card) (a := fun i ↦ if hi : i ∈ Icc 1 ↑(#X) then (f (fx (⟨ i, (by exact hi) ⟩))) else 0)
+  have h {a b c:ℝ} (h1: a ≤ b) (h2 : b = c) : a ≤ c := by linarith
+  apply h this; clear h this
+  apply eq_finite_series
+  intro i hi
+  have hi2 : i ∈ Icc 1 ↑(#X)
+  . simp [hi]
+  simp [hi2]
 
 /-- Lemma 7.1.13 --/
 theorem finite_series_of_finite_series {XX YY:Type*} (X: Finset XX) (Y: Finset YY)
@@ -364,7 +564,12 @@ theorem finite_series_of_finite_series {XX YY:Type*} (X: Finset XX) (Y: Finset Y
     ∑ x ∈ X, ∑ y ∈ Y, f (x, y) = ∑ z ∈ X.product Y, f z := by
   generalize h: X.card = n
   revert X; induction' n with n hn
-  . sorry
+  . intro X hX
+    have hXe : X = ∅
+    . exact card_eq_zero.mp hX
+    rw [hXe]
+    simp only [product_eq_sprod, empty_product]
+    simp only [finite_series_of_empty]
   intro X hX
   have hnon : X.Nonempty := by grind [card_ne_zero]
   choose x₀ hx₀ using hnon.exists_mem
@@ -392,8 +597,22 @@ theorem finite_series_of_finite_series {XX YY:Type*} (X: Finset XX) (Y: Finset Y
       simp at hz ⊢; grind
     _ = _ := by
       symm; convert finite_series_of_disjoint_union _ _
-      . sorry
-      sorry
+      . rw [hunion]
+        simp only [product_eq_sprod]
+        ext ⟨ x, y ⟩
+        simp
+        constructor <;> intro h <;> tauto
+      simp only [product_eq_sprod]
+      rw [Finset.disjoint_iff_inter_eq_empty] at hdisj ⊢
+      ext ⟨ x, y ⟩
+      constructor <;> intro h
+      . simp at h
+        rw [Finset.ext_iff] at hdisj
+        have := (hdisj x).mp (by {
+          simp [h]
+        })
+        nomatch this
+      . nomatch h
 
 /-- Corollary 7.1.14 (Fubini's theorem for finite series)-/
 theorem finite_series_refl {XX YY:Type*} (X: Finset XX) (Y: Finset YY) (f: XX × YY → ℝ) :
